42c0fc0595cdd8eaa686e6d6d9d1b7e0dc0b7523
Sergii Semenov <sergiis@live.com>
2018-06-03 15:13:52 -0700

    Add CloudEvents descriptors for routine transitions

---
+++ Data/CloudEvents/CloudEventsSerialization.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Dasync.CloudEvents
+{
+    public class CloudEventsSerialization
+    {
+        public static JsonSerializerSettings JsonSerializerSettings = new JsonSerializerSettings
+        {
+            ContractResolver = new CamelCasePropertyNamesContractResolver(),
+            NullValueHandling = NullValueHandling.Ignore,
+            DefaultValueHandling = DefaultValueHandling.Ignore
+        };
+
+        public static string Serialize(object @object)
+        {
+            return JsonConvert.SerializeObject(@object, JsonSerializerSettings);
+        }
+    }
+}
---
+++ Data/CloudEvents/DasyncCloudEventsTypes.cs
@@ -0,0 +1,23 @@
+﻿namespace Dasync.CloudEvents
+{
+    public class DasyncCloudEventsTypes
+    {
+        public struct EventType
+        {
+            public string Name;
+            public string Version;
+        }
+
+        public static readonly EventType InvokeRoutine = new EventType
+        {
+            Name = "dasync.routine.invoke",
+            Version = "0.1"
+        };
+
+        public static readonly EventType ContinueRoutine = new EventType
+        {
+            Name = "dasync.routine.continue",
+            Version = "0.1"
+        };
+    }
+}
