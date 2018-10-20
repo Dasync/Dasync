7ec07af07a159d5af46698d7cb97f430cab63227
Serge Semenov <sergiis@live.com>
2018-10-20 15:01:15 -0700

    Support for .NET Core 2.1 - DelayPromise now uses TimerQueueTimer directly instead of Timer

--- Engine/Accessors/TimerExtensions.cs
+++ Engine/Accessors/TimerExtensions.cs
@@ -6,96 +6,26 @@ namespace Dasync.Accessors
 {
     public static class TimerExtensions
     {
-        public static void GetSettings(this Timer timer, out DateTime? startTime, out TimeSpan? initialDelay, out TimeSpan? interval)
-        {
-            var timerHolder = timer.GetType()
-                .GetField("m_timer", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
-                .GetValue(timer);
+        private static readonly FieldInfo _timer =
+            typeof(Timer).GetField("_timer", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance) ??
+            typeof(Timer).GetField("m_timer", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
 
-            var timerQueueTimer = timerHolder.GetType()
-                .GetField("m_timer", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
-                .GetValue(timerHolder);
+        private static readonly Type TimerHolderType = typeof(Timer).GetAssembly().GetType("System.Threading.TimerHolder");
 
-            var m_startTicks = (int)timerQueueTimer.GetType()
-                .GetField("m_startTicks", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
-                .GetValue(timerQueueTimer);
+        private static readonly FieldInfo _TimerHolder_timer =
+            TimerHolderType.GetField("_timer", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance) ??
+            TimerHolderType.GetField("m_timer", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
 
-            var m_dueTimeMs = (uint)timerQueueTimer.GetType()
-                .GetField("m_dueTime", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
-                .GetValue(timerQueueTimer);
+        public static object GetTimerQueueTimer(this Timer timer) =>
+            _TimerHolder_timer.GetValue(_timer.GetValue(timer));
 
-            var m_period = (uint)timerQueueTimer.GetType()
-                .GetField("m_period", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
-                .GetValue(timerQueueTimer);
+        public static void GetSettings(this Timer timer, out DateTime? startTime, out TimeSpan? initialDelay, out TimeSpan? interval) =>
+            TimerQueueTimerAccessor.GetSettings(timer.GetTimerQueueTimer(), out startTime, out initialDelay, out interval);
 
-            var nowTicks = TimerQueueExtensions.TickCount;
-            var now = DateTime.Now;
-            var systemStartTime = now - TimeSpan.FromMilliseconds(nowTicks);
+        public static void GetCallbackAndState(this Timer timer, out TimerCallback callback, out object state) =>
+            TimerQueueTimerAccessor.GetCallbackAndState(timer.GetTimerQueueTimer(), out callback, out state);
 
-            if (m_startTicks != 0 && m_startTicks != int.MaxValue && m_startTicks != int.MinValue)
-            {
-                startTime = systemStartTime + TimeSpan.FromMilliseconds(m_startTicks);
-            }
-            else
-            {
-                startTime = null;
-            }
-
-            if (m_dueTimeMs != uint.MaxValue)
-            {
-                initialDelay = TimeSpan.FromMilliseconds(m_dueTimeMs);
-            }
-            else
-            {
-                initialDelay = null;
-            }
-
-            if (m_period != uint.MaxValue)
-            {
-                interval = TimeSpan.FromMilliseconds(m_period);
-            }
-            else
-            {
-                interval = null;
-            }
-        }
-
-        public static void GetCallbackAndState(this Timer timer, out TimerCallback callback, out object state)
-        {
-            var timerHolder = timer.GetType()
-                .GetField("m_timer", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
-                .GetValue(timer);
-
-            var timerQueueTimer = timerHolder.GetType()
-                .GetField("m_timer", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
-                .GetValue(timerHolder);
-
-            callback = (TimerCallback)timerQueueTimer.GetType()
-                .GetField("m_timerCallback", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
-                .GetValue(timerQueueTimer);
-
-            state = timerQueueTimer.GetType()
-                .GetField("m_state", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
-                .GetValue(timerQueueTimer);
-        }
-
-        public static void SetCallbackAndState(this Timer timer, TimerCallback callback, object state)
-        {
-            var timerHolder = timer.GetType()
-                .GetField("m_timer", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
-                .GetValue(timer);
-
-            var timerQueueTimer = timerHolder.GetType()
-                .GetField("m_timer", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
-                .GetValue(timerHolder);
-
-            timerQueueTimer.GetType()
-                .GetField("m_timerCallback", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
-                .SetValue(timerQueueTimer, callback);
-
-            timerQueueTimer.GetType()
-                .GetField("m_state", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
-                .SetValue(timerQueueTimer, state);
-        }
+        public static void SetCallbackAndState(this Timer timer, TimerCallback callback, object state) =>
+            TimerQueueTimerAccessor.SetCallbackAndState(timer.GetTimerQueueTimer(), callback, state);
     }
 }
