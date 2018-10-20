1e97c634a6008cf022ab00b9b86718474965039f
Serge Semenov <sergiis@live.com>
2018-10-20 13:39:26 -0700

    Support for .NET Core 2.1 - MoveNextRunner has been replaced with AsyncStateMachineBox

---
+++ Engine/Accessors/AsyncStateMachineBoxAccessor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+namespace Dasync.Accessors
+{
+    public static class AsyncStateMachineBoxAccessor
+    {
+        private static readonly Type AsyncStateMachineBoxType =
+            typeof(AsyncStateMachineAttribute).GetAssembly().GetType(
+                "System.Runtime.CompilerServices.AsyncTaskMethodBuilder`1")
+            .GetNestedType("AsyncStateMachineBox`1", BindingFlags.NonPublic);
+
+        private static readonly Type IAsyncStateMachineBoxType =
+            typeof(AsyncStateMachineAttribute).GetAssembly().GetType(
+                "System.Runtime.CompilerServices.IAsyncStateMachineBox");
+
+        // Cannot be pre-defined, because it's generic.
+        //private static readonly FieldInfo _Context =
+        //    AsyncStateMachineBoxType?.GetField("Context",
+        //        BindingFlags.Instance | BindingFlags.Public);
+
+        private static readonly MethodInfo _GetStateMachineObject =
+            IAsyncStateMachineBoxType?.GetMethod("GetStateMachineObject",
+                BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+
+        public static bool IsAsyncStateMachineBox(Type type) =>
+            AsyncStateMachineBoxType != null &&
+            type.IsGenericType() &&
+            AsyncStateMachineBoxType.IsAssignableFrom(type.GetGenericTypeDefinition());
+
+        public static ExecutionContext GetContext(object box)
+        {
+            var fiContext = box.GetType().GetField("Context", BindingFlags.Instance | BindingFlags.Public);
+            return (ExecutionContext)fiContext.GetValue(box);
+        }
+
+        public static IAsyncStateMachine GetStateMachine(object box)
+            => (IAsyncStateMachine)_GetStateMachineObject.Invoke(box, null);
+    }
+}
--- Engine/Accessors/MoveNextRunnerAccessor.cs
+++ Engine/Accessors/MoveNextRunnerAccessor.cs
@@ -9,18 +9,21 @@ namespace Dasync.Accessors
     {
 #warning TODO: pre-compile GetContext and GetStateMachine
 
-        public static readonly Type MoveNextRunnerType =
+        // MoveNextRunner has been replaced with AsyncStateMachineBox starting from .NET Core 2.1
+        private static readonly Type MoveNextRunnerType =
             typeof(AsyncStateMachineAttribute).GetAssembly().GetType(
                 "System.Runtime.CompilerServices.AsyncMethodBuilderCore")
             .GetNestedType("MoveNextRunner", BindingFlags.NonPublic);
 
         private static readonly FieldInfo _fi_m_context =
-            MoveNextRunnerType.GetField("m_context",
+            MoveNextRunnerType?.GetField("m_context",
                 BindingFlags.Instance | BindingFlags.NonPublic);
 
         private static readonly FieldInfo _fi_m_stateMachine =
-            MoveNextRunnerType.GetField("m_stateMachine",
+            MoveNextRunnerType?.GetField("m_stateMachine",
                 BindingFlags.Instance | BindingFlags.NonPublic);
+
+        public static bool IsMoveNextRunner(Type type) => MoveNextRunnerType != null && MoveNextRunnerType.IsAssignableFrom(type);
 
         public static ExecutionContext GetContext(object moveNextRunner)
             => (ExecutionContext)_fi_m_context?.GetValue(moveNextRunner);
