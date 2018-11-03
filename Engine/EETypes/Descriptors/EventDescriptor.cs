61bdd32541c803387995a0f244b5b5db418e596d
Serge Semenov <sergiis@live.com>
2018-11-02 19:10:58 -0700

    Add EE types to support events

---
+++ Engine/EETypes/Descriptors/EventDescriptor.cs
@@ -0,0 +1,24 @@
+﻿namespace Dasync.EETypes.Descriptors
+{
+    public struct EventDescriptor
+    {
+        public ServiceId ServiceId;
+
+        public EventId EventId;
+
+        public override bool Equals(object obj) =>
+            (obj is EventDescriptor eventDesc)
+            ? this == eventDesc
+            : base.Equals(obj);
+
+        public override int GetHashCode() =>
+            (ServiceId != null && EventId != null)
+            ? ServiceId.GetHashCode() ^ EventId.GetHashCode()
+            : base.GetHashCode();
+
+        public static bool operator ==(EventDescriptor a, EventDescriptor b) =>
+            a.ServiceId == b.ServiceId && a.EventId == b.EventId;
+
+        public static bool operator !=(EventDescriptor a, EventDescriptor b) => !(a == b);
+    }
+}
