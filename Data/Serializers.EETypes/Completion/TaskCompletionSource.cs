2cd001ec17618dac6039d36db05d35f638ce4bd2
Serge Semenov <sergiis@live.com>
2018-12-14 21:54:16 -0800

    Ability to serialize TaskCompletionSource

---
+++ Data/Serializers.EETypes/Completion/TaskCompletionSource.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using Dasync.Accessors;
+using Dasync.Serialization;
+using Dasync.ValueContainer;
+
+namespace Dasync.Serializers.EETypes.Completion
+{
+    public class TaskCompletionSourceSerializer : IObjectDecomposer, IObjectComposer
+    {
+        public IValueContainer Decompose(object value)
+        {
+            return new TaskCompletionSourceContainer
+            {
+                Task = TaskCompletionSourceAccessor.GetTask(value)
+            };
+        }
+
+        public object Compose(IValueContainer container, Type valueType)
+        {
+            var values = (TaskCompletionSourceContainer)container;
+
+            if (values.Task == null) // must not be NULL!
+                return TaskCompletionSourceAccessor.Create(typeof(object));
+
+            return TaskCompletionSourceAccessor.Create(values.Task);
+        }
+
+        public IValueContainer CreatePropertySet(Type valueType)
+            => new TaskCompletionSourceContainer();
+    }
+
+    public class TaskCompletionSourceContainer : ValueContainerBase
+    {
+        public Task Task;
+    }
+}
---
+++ Engine/Accessors/TaskCompletionSourceAccessor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Dasync.Accessors
+{
+    public static class TaskCompletionSourceAccessor
+    {
+        public static object Create(Type taskResultType)
+        {
+            var taskCompletionSourceType = typeof(TaskCompletionSource<>).MakeGenericType(taskResultType);
+            return Activator.CreateInstance(taskCompletionSourceType);
+        }
+
+        public static object Create(Task task)
+        {
+            var taskResultType = TaskAccessor.GetTaskResultType(task.GetType());
+            var taskCompletionSource = Create(taskResultType);
+            SetTask(taskCompletionSource, task);
+            return taskCompletionSource;
+        }
+
+        public static Task GetTask(object taskCompletionSource)
+        {
+            var taskField = taskCompletionSource.GetType()
+                .GetField("_task", BindingFlags.NonPublic | BindingFlags.Instance);
+            return (Task)taskField.GetValue(taskCompletionSource);
+        }
+
+        public static void SetTask(object taskCompletionSource, Task task)
+        {
+            var taskField = taskCompletionSource.GetType()
+                .GetField("_task", BindingFlags.NonPublic | BindingFlags.Instance);
+            taskField.SetValue(taskCompletionSource, task);
+        }
+    }
+}
