a0cdd641762d1338118d519e93b432c4303e8481
Serge <sergiis@live.com>
2019-09-09 10:10:26 -0700

    Introduce scoped service provider to dynamically resolve scoped services from singletons.

---
+++ IoC/DependencyInjection/ScopedServiceProvider.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+
+namespace Dasync.DependencyInjection
+{
+    public class ScopedServiceProvider : IScopedServiceProvider
+    {
+        public static readonly AsyncLocal<IServiceProvider> Instance = new AsyncLocal<IServiceProvider>();
+
+        public object GetService(Type serviceType)
+        {
+            var sp = Instance.Value;
+            if (sp == null)
+                throw new InvalidOperationException("A service resolution has been requested outside of a scope.");
+            return sp.GetService(serviceType);
+        }
+    }
+}
