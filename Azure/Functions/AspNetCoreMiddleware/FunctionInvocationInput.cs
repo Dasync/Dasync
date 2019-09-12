55347917f3c6d40c3ef04df9905ebdc33853d7e8
Serge <sergiis@live.com>
2019-09-11 19:46:07 -0700

    Bind scoped function inputs for middleware to inject

---
+++ Azure/Functions/AspNetCoreMiddleware/FunctionInvocationInput.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading;
+using Microsoft.Extensions.Logging;
+using ExecutionContext = Microsoft.Azure.WebJobs.ExecutionContext;
+
+namespace Dasync.Azure.Functions.AspNetCoreMiddleware
+{
+    internal sealed class FunctionInvocationInput
+    {
+        public static AsyncLocal<FunctionInvocationInput> Current = new AsyncLocal<FunctionInvocationInput>();
+
+        public ExecutionContext Context { get; set; }
+
+        public ILogger Logger { get; set; }
+
+        public CancellationToken CancellationToken { get; set; }
+    }
+}
--- Azure/Functions/AspNetCoreMiddleware/WebJobsMiddlewareExtensions.cs
+++ Azure/Functions/AspNetCoreMiddleware/WebJobsMiddlewareExtensions.cs
@@ -1,6 +1,11 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System;
+using System.Linq;
+using System.Threading;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using ExecutionContext = Microsoft.Azure.WebJobs.ExecutionContext;
 
 namespace Dasync.Azure.Functions.AspNetCoreMiddleware
 {
@@ -12,6 +17,21 @@ namespace Dasync.Azure.Functions.AspNetCoreMiddleware
             services.AddSingleton<IHostedService, MiddlewareSupportService>();
             services.EnableRegisteredHostedServices();
             services.AddScoped<IMiddlewareFactory, MiddlewareFactory>();
+            services.AddFunctionInvocationInput();
+            return services;
+        }
+
+        private static IServiceCollection AddFunctionInvocationInput(this IServiceCollection services)
+        {
+            if (!services.Any(d => d.ServiceType == typeof(ExecutionContext)))
+                services.AddScoped(_ => FunctionInvocationInput.Current.Value.Context);
+
+            if (!services.Any(d => d.ServiceType == typeof(ILogger)))
+                services.AddScoped(_ => FunctionInvocationInput.Current.Value.Logger);
+
+            if (!services.Any(d => d.ServiceType == typeof(Func<CancellationToken>)))
+                services.AddScoped<Func<CancellationToken>>(_ => () => FunctionInvocationInput.Current.Value.CancellationToken);
+
             return services;
         }
     }
