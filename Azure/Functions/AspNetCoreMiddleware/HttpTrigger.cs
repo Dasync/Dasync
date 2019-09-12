755048be578d8e9e4b14aab2bbb65140aba3c014
Serge <sergiis@live.com>
2019-09-11 19:46:19 -0700

    Add default HTTP trigger

---
+++ Azure/Functions/AspNetCoreMiddleware/HttpTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ExecutionContext = Microsoft.Azure.WebJobs.ExecutionContext;
+
+namespace Dasync.Azure.Functions.AspNetCoreMiddleware
+{
+    internal static class HttpTrigger
+    {
+        public static Task<IActionResult> Invoke(
+            HttpRequest request,
+            ExecutionContext context,
+            ILogger logger,
+            CancellationToken ct)
+        {
+            FunctionInvocationInput.Current.Value = new FunctionInvocationInput
+            {
+                Context = context,
+                Logger = logger,
+                CancellationToken = ct
+            };
+            return request.ReplyWithMiddlewareAsync();
+        }
+    }
+}
