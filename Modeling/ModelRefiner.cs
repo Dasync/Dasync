aaa9f34905494616042e8d4471a53e45b154ca1e
Serge <sergiis@live.com>
2019-09-16 12:51:15 -0700

    Fix: missing files for auto-find all service methods in model

---
+++ Modeling/ModelRefiner.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Dasync.Modeling
+{
+    public class ModelRefiner
+    {
+        public static void Refine(CommunicationModelBuilder builder)
+        {
+            foreach (var serviceDefinition in builder.Model.Services)
+            {
+                FindMethods(serviceDefinition);
+            }
+        }
+
+        public static void FindMethods(IMutableServiceDefinition serviceDefinition)
+        {
+            if (serviceDefinition.Implementation != null)
+            {
+                foreach (var methodInfo in serviceDefinition.Implementation.GetMethods(
+                    BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance))
+                {
+                    if (serviceDefinition.FindMethod(methodInfo.Name) == null && methodInfo.IsRoutineCandidate())
+                        serviceDefinition.GetMethod(methodInfo.Name);
+                }
+            }
+
+            foreach (var interfaceType in serviceDefinition.Interfaces)
+            {
+                foreach (var methodInfo in interfaceType.GetMethods(
+                    BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance))
+                {
+                    if (serviceDefinition.FindMethod(methodInfo.Name) == null && methodInfo.IsRoutineCandidate())
+                        serviceDefinition.GetMethod(methodInfo.Name);
+                }
+            }
+        }
+    }
+}
---
+++ Modeling/ModelingExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Dasync.Modeling
+{
+    public static class ModelingExtensions
+    {
+        public static bool IsRoutineCandidate(this MethodInfo methodInfo)
+        {
+            if (!methodInfo.IsFamily && !methodInfo.IsPublic)
+                return false;
+
+            if (!methodInfo.IsVirtual)
+                return false;
+
+            if (methodInfo.IsAbstract && !methodInfo.DeclaringType.IsInterface)
+                return false;
+
+            if (methodInfo.ContainsGenericParameters)
+                return false;
+
+            if (!typeof(Task).IsAssignableFrom(methodInfo.ReturnType))
+            {
+                // An exception from the rule for service instances.
+                if (methodInfo.ReturnType == typeof(void) &&
+                    methodInfo.Name == nameof(IDisposable.Dispose) &&
+                    methodInfo.GetParameters().Length == 0)
+                    return true;
+
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
