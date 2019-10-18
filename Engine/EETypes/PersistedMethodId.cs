b5e8d4c0d994d2a350f82e00daf1c0a7d0a828de
Serge <sergiis@live.com>
2019-10-18 13:45:38 -0700

    copy PersistedMethodId

--- Engine/EETypes/PersistedMethodId.cs
+++ Engine/EETypes/PersistedMethodId.cs
@@ -11,5 +11,17 @@
         public string RoutineId { get; set; }
 
         public string ETag { get; set; }
+
+        public override MethodId Clone() => CopyTo(new PersistedMethodId());
+
+        public override T CopyTo<T>(T copy)
+        {
+            base.CopyTo(copy);
+            var x = (PersistedMethodId)(object)copy;
+            x.IntentId = IntentId;
+            x.RoutineId = RoutineId;
+            x.ETag = ETag;
+            return copy;
+        }
     }
 }
