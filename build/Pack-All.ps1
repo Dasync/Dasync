f1592246527043c68e1406662f15a51b44eef04b
Serge <sergiis@live.com>
2019-11-19 16:33:33 -0800

    fix pack script

--- build/Pack-All.ps1
+++ build/Pack-All.ps1
@@ -1,14 +1,20 @@
 pushd
 
-cd Join-Path $PSScriptRoot '..'
+cd $(Join-Path $PSScriptRoot '..')
 
 dotnet build
-dotnet pack
+dotnet pack --no-build
 
-$packagesDirectory = Join-Path $PSScriptRoot 'packages'
-New-Item -Path $PSScriptRoot -Name 'packages' -ItemType 'directory' -Force
+$packagesDirectory = $(Resolve-Path $(Join-Path $PSScriptRoot 'packages')).ToString()
+New-Item -Path $PSScriptRoot -Name 'packages' -ItemType 'directory' -Force | Out-Null
 
 $allPackageFiles = Get-Childitem -Include '*.nupkg','*.snupkg' -File -Recurse -ErrorAction SilentlyContinue
-$allPackageFiles | % { Copy-Item -Path $_.FullName -Destination $packagesDirectory -Force }
+$allPackageFiles | % {
+  $sourceFilePath = $(Resolve-Path $_.FullName).ToString()
+  if (-not $sourceFilePath.StartsWith($packagesDirectory))
+  {
+    Copy-Item -Path $_.FullName -Destination $packagesDirectory -Force
+  }
+}
 
 popd
