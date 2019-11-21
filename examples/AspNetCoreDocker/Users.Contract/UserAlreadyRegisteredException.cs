0825be694e15b9ff65c83a37a125b61ee3c49b3d
Serge <sergiis@live.com>
2019-11-21 12:34:06 -0800

    Add messages and codes to exceptions

--- examples/AspNetCoreDocker/Users.Contract/UserAlreadyRegisteredException.cs
+++ examples/AspNetCoreDocker/Users.Contract/UserAlreadyRegisteredException.cs
@@ -2,5 +2,12 @@
 
 namespace Users.Contract
 {
-    public class UserAlreadyRegisteredException : Exception { }
+    public class UserAlreadyRegisteredException : Exception
+    {
+        public UserAlreadyRegisteredException()
+            : base("The user is already registered")
+        {
+            HResult = 45;
+        }
+    }
 }
--- examples/AspNetCoreDocker/Users.Contract/UserNotFoundException.cs
+++ examples/AspNetCoreDocker/Users.Contract/UserNotFoundException.cs
@@ -2,5 +2,12 @@
 
 namespace Users.Contract
 {
-    public class UserNotFoundException : Exception { }
+    public class UserNotFoundException : Exception
+    {
+        public UserNotFoundException()
+            : base("The user could not be found")
+        {
+            HResult = 46;
+        }
+    }
 }
